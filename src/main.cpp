e4b7792a7a34c69d5a5da7a95c1a93981427e597
Joseph Howarth <josephhowarth17@gmail.com>
2021-01-15 17:42:34 -0600

    Update main code for changes to IMU library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,23 @@
-/*
-FIFO_SPI.ino
-Brian R Taylor
-brian.taylor@bolderflight.com
-
-Copyright (c) 2017 Bolder Flight Systems
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-and associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or 
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
+#include "Arduino.h"
 #include "MPU9250.h"
 
+#define BUTTON_PIN PIN_BUTTON1
 // an MPU9250 object with the MPU-9250 sensor on SPI bus 0 and chip select pin 10
-MPU9250FIFO IMU(SPI,10);
+MPU9250FIFO IMU(SPI,7);
 int status;
+int16_t accData[85*3];
 
 // variables to hold FIFO data, these need to be large enough to hold the data, maximum expected is 85 samples
-float ax[100], ay[100], az[100];
+
+uint8_t fifoSize2;
 size_t fifoSize;
+float ax[100], ay[100], az[100];
 
 void setup() {
   // serial to display data
   Serial.begin(115200);
   while(!Serial) {}
+
 
   IMU.init(); // start communication with IMU   
   IMU.enableAccelFifo(); // enabling the FIFO to record just the accelerometers
@@ -43,27 +26,28 @@ void setup() {
   delay(10); // delay of 10ms should result in an additional 40 samples
   
   IMU.haltSampleAccumulation(); // No addional samples will be placed into the FIFO
-  IMU.readFifo(); // read the fifo buffer from the IMU
+  
+  IMU.readFifoInt(accData,&fifoSize2,2); // read the fifo buffer from the IMU
 
-  // get the X, Y, and Z accelerometer data and their size
-  IMU.getFifoAccelX_mss(&fifoSize,ax);
-  IMU.getFifoAccelY_mss(&fifoSize,ay);
-  IMU.getFifoAccelZ_mss(&fifoSize,az);
+  // // get the X, Y, and Z accelerometer data and their size
+  // IMU.getFifoAccelX_mss(&fifoSize,ax);
+  // IMU.getFifoAccelY_mss(&fifoSize,ay);
+  // IMU.getFifoAccelZ_mss(&fifoSize,az);
   
   // print the data
   Serial.print("The FIFO buffer is ");
-  Serial.print(fifoSize);
+  Serial.print(fifoSize2);
   Serial.println(" samples long.");
-  for (size_t i=0; i < fifoSize; i++) {
-    Serial.print(ax[i],6);
-    Serial.print("\t");
-    Serial.print(ay[i],6);
-    Serial.print("\t");
-    Serial.println(az[i],6);
+  for (size_t i=0; i < fifoSize2; i++) {
+      Serial.printf("X: %5d, Y: %5d, Z: %5d \n",accData[(i*3)],accData[(i*3)+1],accData[(i*3)+2]);
   }
 }
 
-void loop() {}
+void loop() {
+
+
+
+}
 
 
 
